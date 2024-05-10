a9797e63cbd68db12f7e242b5625560d46af2425
Thomas Cionek <thomascionek97@gmail.com>
2024-05-10 16:55:26 -0300

    Remove picture from About section

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,8 +1,14 @@
 import React from "react";
 import Profile from "./Sections/Profile";
+import About from "./Sections/About";
 
 const Home: React.FC = () => {
-  return <Profile />;
+  return (
+    <>
+      <Profile />
+      <About />
+    </>
+  );
 };
 
 export default Home;

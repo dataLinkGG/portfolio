edb018c1ce361a19c8d27535cce1d853b29989b8
Thomas Cionek <thomascionek97@gmail.com>
2024-05-11 18:44:13 -0300

    Justify flex start and refactor hard-coded items

--- src/pages/Sections/Experience.tsx
+++ src/pages/Sections/Experience.tsx
@@ -9,62 +9,21 @@ const Experience: React.FC = () => {
       <div className="experience-details-container">
         <div className="about-containers">
           <div className="details-container">
-            <h2 className="experience-sub-title">Frontend Development</h2>
+            <h2 className="experience-sub-title">Programming Languages</h2>
             <div className="article-container">
               <Skill name="Python" expertise="Experienced" />
               <Skill name="JavaScript" expertise="Experienced" />
               <Skill name="PostegreSQL" expertise="Experienced" />
-              <Skill name="HTML" expertise="Experienced" />
-              <Skill name="CSS" expertise="Experienced" />
             </div>
           </div>
           <div className="details-container">
-            <h2 className="experience-sub-title">Frontend Development</h2>
+            <h2 className="experience-sub-title">Frameworks</h2>
             <div className="article-container">
-              <article>
-                <img
-                  src="./assets/checkmark.png"
-                  alt="Experience icon"
-                  className="icon"
-                />
-                <div>
-                  <h3>PostgreSQL</h3>
-                  <p>Basic</p>
-                </div>
-              </article>
-              <article>
-                <img
-                  src="./assets/checkmark.png"
-                  alt="Experience icon"
-                  className="icon"
-                />
-                <div>
-                  <h3>Node JS</h3>
-                  <p>Intermediate</p>
-                </div>
-              </article>
-              <article>
-                <img
-                  src="./assets/checkmark.png"
-                  alt="Experience icon"
-                  className="icon"
-                />
-                <div>
-                  <h3>Express JS</h3>
-                  <p>Intermediate</p>
-                </div>
-              </article>
-              <article>
-                <img
-                  src="./assets/checkmark.png"
-                  alt="Experience icon"
-                  className="icon"
-                />
-                <div>
-                  <h3>Git</h3>
-                  <p>Intermediate</p>
-                </div>
-              </article>
+              <Skill name="Linux" expertise="Experienced" />
+              <Skill name="Flask" expertise="Experienced" />
+              <Skill name="React" expertise="Experienced" />
+              <Skill name="Postman" expertise="Experienced" />
+              <Skill name="Rest API" expertise="Experienced" />
             </div>
           </div>
         </div>

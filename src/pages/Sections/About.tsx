10c611a904eb32a8d4ed0cb586318f2493994b15
Thomas Cionek <thomascionek97@gmail.com>
2024-05-14 16:35:48 -0300

    Clean up About section

--- src/pages/Sections/About.tsx
+++ src/pages/Sections/About.tsx
@@ -17,58 +17,55 @@ const About: React.FC = () => {
         <p className="section__text__p1">Get To Know More</p>
         <h1 className="title">About Me</h1>
         <div className="section-container">
-          <div className="about-details-container">
-            <div className="about-containers">
-              <Tooltip
-                placement="left"
-                title="Worked with Python, JavaScript, TypeScript, and PostgreSQL. Interactively
+          <div className="about-containers">
+            <Tooltip
+              placement="bottom"
+              title="Worked with Python, JavaScript, TypeScript, and PostgreSQL. Interactively
                       customized React components from Ant Design based on the product models
                       of Figma, working in Scrum methodology, with Code Reviews, product and
                       team retrospectives meetings. Engagingly implemented back-end library
                       functionalities and actively interacted with the database."
-              >
-                <div className="details-container">
-                  <FontAwesomeIcon icon={faBriefcase} />
-                  <h3>Experience</h3>
+            >
+              <div className="details-container">
+                <FontAwesomeIcon icon={faBriefcase} />
+                <h3>Experience</h3>
+                <br />
+                <p>
+                  {jobTitle}
+                  <br />
+                  Alude YC W20
+                  <br />
+                </p>
+              </div>
+            </Tooltip>
+            <Tooltip
+              placement="bottom"
+              title={`Solid foundation in Linux, having used various distributions for nearly 10 years, with a keen interest in experimenting with commands, customization, and troubleshooting. Gained international exposure during a 4-year stint in Poland, where he studied Computer Science and interacted with exchange students from around the world.`}
+            >
+              <div className="details-container">
+                <div>
+                  <FontAwesomeIcon icon={faGraduationCap} />
+                  <h3>Education</h3>
+                  <br />
+                  <p>Análise e Desenvolvimento de Sistemas</p>
+                  <br />
+                </div>
+              </div>
+            </Tooltip>
+            <Tooltip>
+              <div className="details-container">
+                <div>
+                  <FontAwesomeIcon icon={faBullseye} />
+                  <h3>Goal</h3>
                   <br />
                   <p>
-                    {jobTitle}
-                    <br />
-                    Alude YC W20
-                    <br />
+                    Seeking new junior-level opportunities as a Front End, Back
+                    end or Full Stack developer
                   </p>
+                  <br />
                 </div>
-              </Tooltip>
-              <br />
-              <Tooltip
-                placement="right"
-                title={`Solid foundation in Linux, having used various distributions for nearly 10 years, with a keen interest in experimenting with commands, customization, and troubleshooting. Gained international exposure during a 4-year stint in Poland, where he studied Computer Science and interacted with exchange students from around the world.`}
-              >
-                <div className="details-container">
-                  <div>
-                    <FontAwesomeIcon icon={faGraduationCap} />
-                    <h3>Education</h3>
-                    <p>
-                      Análise e Desenvolvimento de Sistemas
-                      <br /> (August 2022 - August 2024)
-                      <br />
-                    </p>
-                    <br />
-                  </div>
-                </div>
-              </Tooltip>
-            </div>
-            <div className="details-container">
-              <div>
-                <FontAwesomeIcon icon={faBullseye} />
-                <h3>Goal</h3>
-                <p>
-                  Seeking new junior-level opportunities as a Front End, Back
-                  end or Full Stack developer
-                </p>
-                <br />
               </div>
-            </div>
+            </Tooltip>
           </div>
         </div>
       </div>

fbc449a6a9ed3856a52f404bc1040bcdc4bc2db1
Thomas Cionek <thomascionek97@gmail.com>
2024-05-14 18:36:20 -0300

    Improve about section

--- src/pages/Sections/Contact.tsx
+++ src/pages/Sections/Contact.tsx
@@ -1,26 +1,29 @@
 import { Button, Tooltip } from "antd";
 
 const Contact = () => {
-  const myNumber = " +55 (41) 98840-9682";
   const linkToWhatsAppChat = "https://wa.me/5541988409682";
 
   return (
     <section id="contact">
       <p className="section__text__p1">Get in Touch</p>
       <h1 className="title">Contact Me</h1>
-      <div>
-        <img
-          className="qr-code"
-          src="src/assets/qr_black_white_rounded.png"
-          alt=""
-        />
-      </div>
-      <Tooltip placement="right" title={"Chat with me!"}>
-        <Button type="text">
-          <a href={linkToWhatsAppChat} target="_blank">
-            {myNumber}
-          </a>
+      <Tooltip placement="right" title={"Click or scan to chat with me!"}>
+        <Button
+          type="text"
+          target="_blank"
+          onClick={() => {
+            window.open(linkToWhatsAppChat, "_blank");
+          }}
+        >
+          <div>
+            <img
+              className="qr-code"
+              src="src/assets/qr_black_white_rounded.png"
+              alt=""
+            />
+          </div>
         </Button>
+        <br /> {/* just put some element so the tooltip apears in the middle */}
       </Tooltip>
     </section>
   );

818461aec3d03ca824155739b589b226cade22f7
Thomas Cionek <thomascionek97@gmail.com>
2024-05-14 21:13:01 -0300

    Update iconlinkprops

--- src/components/IconLink/IconLink.tsx
+++ src/components/IconLink/IconLink.tsx
@@ -1,10 +1,10 @@
 import { ReactElement } from "react";
 
 type IconLinkProps = {
-  url: string; // Simplified type for the URL
-  altText?: string; // Prop for image alt text
-  className?: string; // Optional prop for image class
-  children?: ReactElement; // Optional children prop for flexibility
+  url: string;
+  altText?: string;
+  className?: string;
+  children?: ReactElement;
 };
 
 const IconLink: React.FC<IconLinkProps> = ({
